d6b4d79c87deeb420f82c789d04a284f091b280b
dev-mahir <mdmahir.ds@gmail.com>
2024-11-03 18:08:38 +0600

    updated eslint config

--- app/learn/tailwind-css/page.jsx
+++ app/learn/tailwind-css/page.jsx
@@ -5,7 +5,7 @@ const Tailwind = () => {
 				Get started with Tailwind CSS Tailwind CSS works by scanning all
 				of your HTML files, JavaScript components, and any other
 				templates for class names, generating the corresponding styles
-				and then writing them to a static CSS file. It's fast, flexible,
+				and then writing them to a static CSS file. It&apos;s fast, flexible,
 				and reliable — with zero-runtime.
 			</div>
 		</>
